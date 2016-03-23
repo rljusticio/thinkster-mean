39d1849dda281c4ecab836d5afda03897fc2aa70
rljusticio <j.donnell.walters@gmail.com>
2016-03-22 22:02:38 -0400

    added backend examples

--- better-angular/ang-overview/backend/finance3.js
+++ better-angular/ang-overview/backend/finance3.js
@@ -1,31 +1,32 @@
-angular.module('finance3', [])
-  .factory('currencyConverter', ['$http', function($http) {
-    var YAHOO_FINANCE_URL_PATTERN = '//query.yahooapis.com/v1/public/yql?q=select * from '+
-        'yahoo.finance.xchange where pair in ("PAIRS")&format=json&'+
-        'env=store://datatables.org/alltableswithkeys&callback=JSON_CALLBACK';
-    var currencies = ['USD', 'EUR', 'CNY'];
-    var usdToForeignRates = {};
+(function(angular){
+  'use strict';
+  angular.module('finance3', [])
+    .factory('currencyConverter', ['$http', function($http) {
+      var YAHOO_FINANCE_URL_PATTERN = '//query.yahooapis.com/v1/public/yql?q=select * from ' + 'yahoo.finance.xchange where pair in ("PAIRS")&format=json&' + 'env=store://datatables.org/alltableswithkeys&callback=JSON_CALLBACK';
+      var currencies = ['USD', 'EUR', 'CNY'];
+      var usdToForeignRates = {};
 
-    var convert = function(amount, inCurr, outCurr) {
-      return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
-    }
+      var convert = function(amount, inCurr, outCurr) {
+        return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+      }
 
-    var refresh = function() {
-      var url = YAHOO_FINANCE_URL_PATTERN.replace('PAIRS', 'USD' + currencies.join('","USD'));
-      return $http.jsonp(url).then(function(response){
-        var newUsdToForeignRates = {};
-        angular.forEach(response.data.query.results.rate, function(rate){
-          var currency = rate.id.substring(3,6);
-          newUsdToForeignRates[currency] = window.parseFloat(rate.Rate);
+      var refresh = function() {
+        var url = YAHOO_FINANCE_URL_PATTERN.replace('PAIRS', 'USD' + currencies.join('","USD'));
+        return $http.jsonp(url).then(function(response){
+          var newUsdToForeignRates = {};
+          angular.forEach(response.data.query.results.rate, function(rate){
+            var currency = rate.id.substring(3,6);
+            newUsdToForeignRates[currency] = window.parseFloat(rate.Rate);
+          });
+          usdToForeignRates = newUsdToForeignRates;
         });
-        usdToForeignRates = newUsdToForeignRates;
-      });
-    }
+      }
 
-    refresh();
+      refresh();
 
-    return {
-      currencies: currencies,
-      convert: convert
-    };
-  }]);
+      return {
+        currencies: currencies,
+        convert: convert
+      };
+    }]);
+})(window.angular);
